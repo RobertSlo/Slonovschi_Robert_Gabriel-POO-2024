9433658412686a230e2d72fe1e973cd122a6b562
RobertSlo <162808756+RobertSlo@users.noreply.github.com>
2024-03-16 13:26:50 +0200

    Add files via upload

---
+++ Laborator01 Problema02 Animale/Animale/Animale.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+
+using namespace System;
+
+ref class Animal
+{
+public:
+    int legs;
+private:
+    String^ name;
+public:
+    void SetName(String^ nm)
+    {
+        name = nm;
+    }
+
+    String^ GetName()
+    {
+        return name;
+    }
+};
+
+int main(array<System::String^>^ args)
+{
+    Console::WriteLine("Hello World");
+    Animal^ cat = gcnew Animal();
+    Animal^ dog = gcnew Animal();
+    cat->SetName("Cat");
+    cat->legs = 4;
+    dog->SetName("Dog");
+    dog->legs = 4;
+    Console::WriteLine("Animal 1");
+    Console::WriteLine("Name: ");
+    Console::WriteLine(cat->GetName());
+    Console::WriteLine("Legs:   ");
+    Console::WriteLine(cat->legs);
+    Console::WriteLine();
+    Console::WriteLine("Animal 2");
+    Console::WriteLine("Name:   ");
+    Console::WriteLine(dog->GetName());
+    Console::WriteLine("Legs:   ");
+    Console::WriteLine(dog->legs);
+    Console::WriteLine();
+
+    return 0;
+}
